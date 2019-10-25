f10128569bfbfe08a4691148c6d45032d94579ec
jsh0061 <jsh@nomadconnextion.com>
2019-10-25 11:53:42 +0900

    20191026_템플릿연결확인중

--- src/main/java/com/tutorial/springboard/service/BoardService.java
+++ src/main/java/com/tutorial/springboard/service/BoardService.java
@@ -1,9 +1,35 @@
 // 게시글 리스트뿐만 아니라 게시글 하나에 대한 요청도 처리할 수 있게 BoardService 클래스 작성
 package com.tutorial.springboard.service;
 
+//import com.tutorial.springboard.domain.Board;
+//import com.tutorial.springboard.repository.BoardRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.data.domain.Page;
+//import org.springframework.data.domain.PageRequest;
+//import org.springframework.data.domain.Pageable;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class BoardService {
+//
+//    @Autowired
+//    private BoardRepository boardRepository;
+//
+//    public Page<Board> findBoardList(Pageable pageable) {
+//        pageable = PageRequest.of(
+//                pageable.getPageNumber() <= 0?0:pageable.getPageNumber()-1,
+//                pageable.getPageSize());
+//        return boardRepository.findAll(pageable);
+//    }
+//
+//    public Board findBoardByIdx(Long idx){
+//        return boardRepository.findById(idx).orElse(new Board());
+//    }
+//
+//}
+
 import com.tutorial.springboard.domain.Board;
 import com.tutorial.springboard.repository.BoardRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -12,18 +38,26 @@ import org.springframework.stereotype.Service;
 @Service
 public class BoardService {
 
-    @Autowired
-    private BoardRepository boardRepository;
+    private final BoardRepository boardRepository;
 
-    public Page<Board> findBoardList(Pageable pageable) {
-        pageable = PageRequest.of(
-                pageable.getPageNumber() <= 0?0:pageable.getPageNumber()-1,
-                pageable.getPageSize());
+    public BoardService(BoardRepository boardRepository){
+        this.boardRepository = boardRepository;
+    }
+
+    public Page<Board> findBoardList(Pageable pageable){
+
+        pageable = PageRequest.of(pageable.getPageNumber() <= 0 ? 0 : pageable
+                .getPageNumber() - 1, pageable.getPageSize());
+
         return boardRepository.findAll(pageable);
     }
 
     public Board findBoardByIdx(Long idx){
+
         return boardRepository.findById(idx).orElse(new Board());
+
     }
+
+
 
 }
